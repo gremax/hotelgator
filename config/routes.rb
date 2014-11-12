4cd2d3bdba543aaaa4fe0a40d1444156367181dd
Max Grechko <max@grechko.org>
2014-11-12 11:27:58 +0200

    Add user signin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get  'signup' => 'users#new'
-  post 'signup' => 'users#create'
+  get    'signup' => 'users#new'
+  post   'signup' => 'users#create'
+  get    'login'  => 'sessions#new'
+  post   'login'  => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   resources :users, only: [:show]
 end
