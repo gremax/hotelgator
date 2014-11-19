29517f05c98223aa52cae68b369e45888e7e7da5
Max Grechko <max@grechko.org>
2014-11-19 19:34:07 +0200

    Add a title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 module ApplicationHelper
+  def full_title(page_title)
+    base_title = "Hotelgator"
+    if page_title.empty?
+      "#{base_title}"
+    else
+      "#{base_title} · #{page_title}"
+    end
+  end
+  
   def boolean_human(boolean)
     if boolean
       "Yes"
